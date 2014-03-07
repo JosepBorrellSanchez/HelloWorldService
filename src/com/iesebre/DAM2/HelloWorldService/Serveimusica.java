d687639816fe3e086e566730d43b819a28a814e2
josepborrellsanchez <josepborrellsanchez@iesebre.com>
2014-03-07 17:01:16 +0100

    fa el servei correctament

---
+++ src/com/iesebre/DAM2/HelloWorldService/Serveimusica.java
@@ -0,0 +1,16 @@
+package com.iesebre.DAM2.HelloWorldService;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class Serveimusica extends Service {
+	public Serveimusica() {
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO: Return the communication channel to the service.
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+}

e70c4d0d9aa037d4ba3172aef1dcccd5beae44c8
Josep Borrell Sánchez <josepborrell@iesebre.com>
2014-03-08 20:31:09 +0100

    codi arreglat

---
+++ src/com/iesebre/DAM2/HelloWorldService/MyService.java
@@ -0,0 +1,25 @@
+package com.iesebre.DAM2.HelloWorldService;
+
+import android.app.Service;
+import android.os.IBinder;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MyService extends Service {
+   @Override
+   public IBinder onBind(Intent arg0) {
+      return null;
+   }
+
+   @Override
+   public int onStartCommand(Intent intent, int flags, int startId) {
+      // Let it continue running until it is stopped.
+      Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+      return START_STICKY;
+   }
+   @Override
+   public void onDestroy() {
+      super.onDestroy();
+      Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
+   }
+}
